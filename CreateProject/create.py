b82283d1e70a04469cd9d1e4c7f91067c1df4de2
DevilMayCry4 <237326369@163.com>
2015-03-12 16:43:24 +0800

    v
    nb

--- CreateProject/create.py
+++ CreateProject/create.py
@@ -3,8 +3,15 @@ import shutil
 import errno
 import sys
 import string
-from mod_pbxproj import XcodeProject
+import Foundation, objc,AppKit
+import uuid
 
+#from mod_pbxproj import XcodeProject
+
+NSMutableDictionary = objc.lookUpClass('NSMutableDictionary')
+NSMutableArray = objc.lookUpClass('NSMutableArray')
+NSDictionary = objc.lookUpClass('NSDictionary')
+NSArray = objc.lookUpClass('NSArray')
 
 ProjectName = 'Example'
 
@@ -39,7 +46,7 @@ def cleanPath(path,newProjectName):
                         try:
                             s = open(filePath).read()
                         except IOError:
-                            print "Could not open file! Please close Excel!"
+                            print ("Could not open file! Please close Excel!")
                         else:
                             s = s.replace(ProjectName,newProjectName)
                             f = open(filePath, 'w')
@@ -52,18 +59,154 @@ def addZxing(projectFile):
     currentPath = os.getcwd()
     zxingDir = os.path.join(currentPath,'zxing')
     if os.path.exists(zxingDir):
-         project = XcodeProject.Load(projectFile)
+         project = NSMutableDictionary.dictionaryWithContentsOfFile_(projectFile)
+
+         rootId = project.objectForKey_('rootObject')
+         objects =  project.objectForKey_('objects')
+         PBXProject = objects.objectForKey_(rootId)
+         ProductGroup = createId()
+         ProjectRef = createId()
+         PBXProject.setValue_forKey_([{'ProductGroup':ProductGroup,'ProjectRef':ProjectRef}],'projectReferences')
+         PBXReferenceProxy1 = createId()
+
+         remoteRefId1 = createId()
+
+         remoteRef1 = {
+             'isa':'PBXContainerItemProxy',
+             'containerPortal': ProjectRef,
+             'proxyType' : '2',
+             'remoteGlobalIDString' : createId(),
+             'remoteInfo' : 'ZXingWidget',
+         }
+         objects.setValue_forKey_(remoteRef1,remoteRefId1)
+
+         PBXReferenceProxy1Object = {
+             'isa':'PBXReferenceProxy',
+             'fileType':'archive.ar',
+             'path':'libZXingWidget.a',
+             'remoteRef':remoteRefId1,
+             'sourceTree':'BUILT_PRODUCTS_DIR',
+         }
+         objects.setValue_forKey_(PBXReferenceProxy1Object,PBXReferenceProxy1)
+
+         remoteRefId2 = createId()
+
+         remoteRef2 = {
+             'isa':'PBXContainerItemProxy',
+             'containerPortal': ProjectRef,
+             'proxyType' : '2',
+             'remoteGlobalIDString' : createId(),
+             'remoteInfo' : 'ZXingTests',
+         }
+         objects.setValue_forKey_(remoteRef2,remoteRefId2)
+
+         PBXReferenceProxy2Object = {
+             'isa':'PBXReferenceProxy',
+             'fileType':'wrapper.cfbundle',
+             'path':'ZXingTests.octest',
+             'remoteRef':remoteRefId2,
+             'sourceTree':'BUILT_PRODUCTS_DIR',
+         }
+
+         PBXReferenceProxy2 = createId()
+
+         objects.setValue_forKey_(PBXReferenceProxy2Object,PBXReferenceProxy2)
+
+
+
+         ProductGroupObject = {
+             'isa':'PBXGroup',
+             'children':[PBXReferenceProxy1,PBXReferenceProxy2],
+             'name':'Products',
+             'sourceTree':'<group>'
+                               }
+         objects.setValue_forKey_(ProductGroupObject,ProductGroup)
+
+         ProjectRefObject = {
+             'isa' : 'PBXFileReference',
+             'lastKnownFileType' : 'wrapper.pb-project',
+             'name' : 'ZXingWidget.xcodeproj',
+             'path' :'zxing/iphone/ZXingWidget/ZXingWidget.xcodeproj',
+             'sourceTree' : '<group>',
+         }
+
+         objects.setValue_forKey_(ProjectRefObject,ProjectRef)
+
+         mainGroupId =  PBXProject.objectForKey_('mainGroup')
+         PBXGroup = objects.objectForKey_(mainGroupId)
+         children = PBXGroup.objectForKey_('children')
+         children.insertObject_atIndex_(ProjectRef,0)
+
+         #添加Build Phase ZxingWidget依赖
+         zxingWidgetDependencyId = createId()
+         targets = PBXProject.objectForKey_('targets')
+         projectTargetId = targets[0]
+         PBXNativeTarget = objects.objectForKey_(projectTargetId)
+         dependencies = PBXNativeTarget.objectForKey_('dependencies')
+         dependencies.addObject_(zxingWidgetDependencyId)
+         print(dependencies)
+
+         targetProxyId = createId()
+
+         zxingWidgetDependency = {
+             'isa' : 'PBXTargetDependency',
+             'name' : 'ZXingWidget',
+             'targetProxy':targetProxyId,
+         }
+         objects.setValue_forKey_(zxingWidgetDependency,zxingWidgetDependencyId)
+
+         targetProxy = {
+             'isa' : 'PBXContainerItemProxy',
+             'containerPortal' : ProjectRef,
+             'proxyType' : '2',
+             'remoteGlobalIDString': createId(),
+             'remoteInfo' : 'ZXingWidget',
+         }
+         objects.setValue_forKey_(targetProxy,targetProxyId)
+
+
+
+
+
+
+         if project.writeToFile_atomically_(projectFile +'tst',True):
+             print('save')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else:
+        print ('zxing not exist')
+
+
+def createId():
+    return ''.join(str(uuid.uuid4()).upper().split('-')[1:])
 
 
 
 if __name__ == '__main__':
 
-    project = XcodeProject.Load('/Users/virgil/Documents/python/CreateProject/testvv/testvv.xcodeproj/project.pbxproj')
-    frameWork = project.get_or_create_group('framework')
-
-    new_group = project.get_or_create_group('testvv')
-    project.add_file('/Users/virgil/Desktop/te.m', parent=new_group)
-    project.save()
+    addZxing('/Users/virgil/Desktop/project.pbxproj')
     '''
     if len(sys.argv) > 1:
             newProjectName = sys.argv[1]
